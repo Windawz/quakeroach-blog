80493b5aabcf70f9c1cafe63ce7d1d3dcf92676d
Windawz <halfer2000@gmail.com>
2024-10-05 19:48:51 +0200

    Add main page and banner

---
+++ quakeroach-blog-frontend/src/pages/main-page/MainPage.tsx
@@ -0,0 +1,10 @@
+import Banner from './components/Banner';
+import './MainPage.css'
+
+export default function MainPage() {
+  return (
+    <div className="main-page">
+      <Banner />
+    </div>
+  );
+}
---
+++ quakeroach-blog-frontend/src/pages/main-page/components/Banner.tsx
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner-text">Quakeroach Blog</div>
+    </div>
+  );
+}
