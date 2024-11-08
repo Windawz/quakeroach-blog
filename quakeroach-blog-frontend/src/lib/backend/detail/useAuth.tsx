8d4f788c71dbb8f0de1fe9ef13d0a5a92b5e4865
Windawz <halfer2000@gmail.com>
2024-11-08 10:16:14 +0100

    Add hook for auth

---
+++ quakeroach-blog-frontend/src/lib/backend/detail/useAuth.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { useApiState } from "./apiState";
+import { apiCall } from "./apiCall";
+
+export function useAuth(): AuthController {
+  const {
+    tokenPair,
+    setTokenPair,
+    userName,
+    setUserName,
+  } = useApiState();
+
+  const [inputUserName, setInputUserName] = useState<string>("");
+  const [inputPasswordText, setInputPasswordText] = useState<string>("");
+
+  const isAuthenticated = (): boolean => {
+    return tokenPair !== undefined && userName !== undefined;
+  }
+
+  const submitInput = async (): Promise<AuthSubmitResult> => {
+    if (isAuthenticated()) {
+      return {
+        kind: "alreadyAuthenticated",
+      };
+    }
+
+    const response = await apiCall({
+      intent: "authenticate",
+      userName: inputUserName,
+      passwordText: inputPasswordText,
+    });
+
+    switch (response.kind) {
+      case "success":
+        setTokenPair(response.tokens);
+        setUserName(inputUserName);
+        return {
+          kind: "success",
+        };
+      case "error":
+        return {
+          kind: "error",
+          message: response.message,
+          status: response.status,
+        };
+    }
+  }
+
+  const getAuthInfo = (): AuthInfo => {
+    return isAuthenticated()
+      ? {
+        isAuthenticated: true,
+        userName: userName!,
+      }
+      : {
+        isAuthenticated: false,
+      };
+  }
+
+  return {
+    inputUserName,
+    setInputUserName,
+    inputPasswordText,
+    setInputPasswordText,
+    submitInput,
+    getAuthInfo,
+  };
+}
+
+export interface AuthController {
+  inputUserName: string;
+  setInputUserName: (value: string) => void;
+  inputPasswordText: string;
+  setInputPasswordText: (value: string) => void;
+  submitInput: () => Promise<AuthSubmitResult>;
+  getAuthInfo: () => AuthInfo;
+}
+
+export type AuthInfo = AuthenticatedAuthInfo | UnauthenticatedAuthInfo;
+
+export interface AuthenticatedAuthInfo {
+  isAuthenticated: true;
+  userName: string;
+}
+
+export interface UnauthenticatedAuthInfo {
+  isAuthenticated: false;
+}
+
+export type AuthSubmitResult = SuccessAuthSubmitResult | AlreadyAuthenticatedAuthSubmitResult | ErrorAuthSubmitResult;
+
+export interface SuccessAuthSubmitResult {
+  kind: "success";
+}
+
+export interface AlreadyAuthenticatedAuthSubmitResult {
+  kind: "alreadyAuthenticated";
+}
+
+export interface ErrorAuthSubmitResult {
+  kind: "error";
+  message: string;
+  status: number;
+}
