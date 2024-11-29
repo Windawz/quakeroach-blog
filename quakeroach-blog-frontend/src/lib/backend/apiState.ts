20f1309bd1187cebf8f652f31e9ee81b1344b558
Windawz <halfer2000@gmail.com>
2024-11-29 12:29:58 +0100

    Pass the value of tokensReceivedAt obtained from cookie to moment()

--- quakeroach-blog-frontend/src/lib/backend/apiState.ts
+++ quakeroach-blog-frontend/src/lib/backend/apiState.ts
@@ -1,6 +1,7 @@
 import { Moment } from "moment";
 import { TokenPair } from "./TokenPair";
 import { useCookies } from "react-cookie";
+import moment from "moment";
 
 export interface ApiStateController {
   apiState?: ApiState;
@@ -17,7 +18,16 @@ export function useApiState(): ApiStateController {
   const cookieName = "quakeroach-blog";
   const [cookies, setCookie, removeCookie] = useCookies([cookieName]);
 
-  const apiState = cookies[cookieName];
+  const cookie = cookies[cookieName];
+
+  const apiState: ApiState | undefined = cookie === undefined
+    ? undefined
+    : {
+      userName: cookie.userName,
+      tokens: cookie.tokens,
+      tokensReceivedAt: moment(cookie.tokensReceivedAt),
+    };
+  
   const setApiState = (value: ApiState | undefined) => value !== undefined
     ? setCookie(cookieName, value)
     : removeCookie(cookieName);
