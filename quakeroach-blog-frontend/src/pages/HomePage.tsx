d483ebe312a3c25c10e923ad0405ca630b3d6efa
Windawz <halfer2000@gmail.com>
2024-12-10 13:15:30 +0100

    Make home page pending and no blog posts messages use containers

--- quakeroach-blog-frontend/src/pages/HomePage.tsx
+++ quakeroach-blog-frontend/src/pages/HomePage.tsx
@@ -2,46 +2,49 @@ import './styles/HomePage.css'
 import BlogPostShortView from '../components/BlogPostShortView';
 import { useBlogPosts } from '../lib/backend/queries';
 import { AppError } from '../lib/errorHandling';
+import { QueryResult } from '../lib/backend/apiHooks';
+import { BlogPost } from '../lib/data/BlogPost';
+import Container from '../components/Container';
 
 export default function HomePage() {
   const queryResult = useBlogPosts({
     maxCount: 10,
   });
 
-  let contents = undefined;
-
-  switch (queryResult.kind) {
-    case "success":
-      const blogPosts = queryResult.data;
-
-      if (blogPosts.length === 0) {
-        contents = (
-          <div className="box">
-            No blog posts to show
-          </div>
-        );
-      } else {
-        contents = blogPosts.map(x => (
-          <BlogPostShortView blogPost={x} />
-        ));
-      }
-      break;
-    case "pending":
-      contents = (
-        <div className="box">
-          Pending...
-        </div>
-      );
-      break;
-    case "error":
-      throw new AppError({
-        message: queryResult.message,
-      });
-  }
+  let contents = getHomePageContents(queryResult);
 
   return (
     <div className="home-page">
       {contents}
     </div>
   );
+}
+
+function getHomePageContents(queryResult: QueryResult<BlogPost[]>): JSX.Element | JSX.Element[] {
+  switch (queryResult.kind) {
+    case "success":
+      const blogPosts = queryResult.data;
+
+      if (blogPosts.length === 0) {
+        return (
+          <Container>
+            No blog posts to show
+          </Container>
+        );
+      } else {
+        return blogPosts.map(x => (
+          <BlogPostShortView blogPost={x} />
+        ));
+      }
+    case "pending":
+      return (
+        <Container>
+          Pending...
+        </Container>
+      );
+    case "error":
+      throw new AppError({
+        message: queryResult.message,
+      });
+  }
 }
