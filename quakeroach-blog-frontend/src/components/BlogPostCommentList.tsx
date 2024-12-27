a5312fa473ae96dc98119f741019e128e20d4652
Windawz <halfer2000@gmail.com>
2024-12-27 15:26:02 +0100

    Add barebones implementation of blog post comment list

--- quakeroach-blog-frontend/src/components/BlogPostCommentList.tsx
+++ quakeroach-blog-frontend/src/components/BlogPostCommentList.tsx
@@ -1,3 +1,7 @@
+import { useComments } from "../lib/backend/queries";
+import { AppError } from "../lib/errorHandling";
+import Container from "./Container";
+
 export interface BlogPostCommentListProps {
   blogPostId: number;
 }
@@ -5,8 +9,46 @@ export interface BlogPostCommentListProps {
 export default function BlogPostCommentList({
   blogPostId,
 }: BlogPostCommentListProps) {
+  const commentsResult = useComments({
+    blogPostId,
+    maxCount: 10,
+  });
+
+  let main = (
+    <Container>
+      Pending...
+    </Container>
+  );
+
+  if (commentsResult.kind === "error") {
+    throw new AppError({
+      message: commentsResult.message,
+    });
+  }
+
+  if (commentsResult.kind === "success") {
+    const comments = commentsResult.data.map(x => (
+      <Container className="comment">
+        {x.contents}
+      </Container>
+    ));
+
+    if (comments.length === 0) {
+      main = (
+        <Container className="no-comments-container">
+          No one has commented on this post yet
+        </Container>
+      );
+    } else {
+      main = (
+        <>{comments}</>
+      );
+    }
+  }
+
   return (
     <div className="blog-post-comment-list">
+      {main}
     </div>
   );
 }
